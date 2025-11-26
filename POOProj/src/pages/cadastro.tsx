e751068f36d9eecec2ea8cff5f6277fe985c29c7
AntonioCarlosPereira <4nt.556@gmail.com>
2025-11-26 10:37:10 -0300

    cadastro funcionando

--- POOProj/src/pages/cadastro.tsx
+++ POOProj/src/pages/cadastro.tsx
@@ -1,13 +1,13 @@
-import "./cadastro.css";
 import { Link } from "react-router-dom";
+import "./Cadastro.css";
 
 export default function Cadastro() {
   return (
-    <div className="popup-overlay">
-      <div className="popup-box">
+    <div className="cadastro-container">
+      <div className="cadastro-card">
         <h2>Cadastro</h2>
 
-        <label>Usuario:</label>
+        <label>Usuário:</label>
         <input type="text" />
 
         <label>Senha:</label>
@@ -20,12 +20,12 @@ export default function Cadastro() {
         <div className="radio-group">
           <label><input type="radio" name="tipo" /> Cliente</label>
           <label><input type="radio" name="tipo" /> Prestador de Serviço</label>
-          <label><input type="radio" name="tipo" /> Proprietario</label>
+          <label><input type="radio" name="tipo" /> Proprietário</label>
         </div>
 
         <button className="submit-btn">Finalizar Cadastro</button>
 
-        <Link to="/" className="close-link">Fechar</Link>
+        <Link to="/" className="voltar-link">Voltar</Link>
       </div>
     </div>
   );
